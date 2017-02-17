7b278acb2e06b6e2f9d52be721d823fcf54dd7e0
Felipe Zapata <fzapata@Felipes-MacBook-Air.local>
2017-02-16 17:00:21 -0800

    new files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,43 @@
-<script type="text/javascript" src="myscripts.js"></script>
+	var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-var countryNames = ["germany", "italy", "france", "norway", "argentina", "england", "afghanistan", "azerbaïdjan", "bangladesh", "peru", "cambodia", "colombia", "denmark", "ecuador", "greenland", "guatemala", "hungary", "kazakhstan", "lebanon", "luxembourg", "adagascar", "montenegro", "netherlands", "paksitan", "paraguay", "uruguay", "philippines", "brazil", "romania", "spain", "switzerland", "turkmenistan", "venezuela", "bolivia", "zimbabwe"]
-var randomWord = countryNames[Math.floor(Math.random() * countryNames.lenght)];
+	var wins = 0;
+	var losses = 0;
+	var guessesleft = 9;
+	var lettersUsed = [];
+
+ 	document.onkeyup = function(event){
+	var usersGuess = event.key;
+	var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+   	if (usersGuess === computerGuess){
+    wins++;
+    alert('You Win!');
+ }
+ 	if (guessesleft == 1){
+   	guessesleft = 9;
+    lettersUsed = [];
+    losses++;
+   	alert('You Lose!');
+ }
+
+ {
+    if (guessesleft !== 0){
+    guessesleft--;
+    lettersUsed.push(usersGuess);
+     }
+
+     else {
+       losses++;
+       guessesleft = 9;
+       lettersUsed = [];
+     }
+ }
+   
+	var html = "<p>Wins: " + wins + "</p>" +
+         	   "<p>Losses: " + losses + "</p>" +
+         	   "<p>Guesses Left: " + guessesleft + "</p>" +
+         	   "<p>Your Guesses so far:" + lettersUsed + "</p>";
+
+    document.querySelector("#game").innerHTML = html;
+ }
+     
