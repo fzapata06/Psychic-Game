75e115314c843ece0c16e7e52b35dbf91575b391
Felipe Zapata <fzapata@Felipes-MacBook-Air.local>
2017-02-16 16:09:10 -0800

    updated files

---
+++ assets/javascript/game.js
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="myscripts.js"></script>
+
+var countryNames = ["germany", "italy", "france", "norway", "argentina", "england", "afghanistan", "azerbaïdjan", "bangladesh", "peru", "cambodia", "colombia", "denmark", "ecuador", "greenland", "guatemala", "hungary", "kazakhstan", "lebanon", "luxembourg", "adagascar", "montenegro", "netherlands", "paksitan", "paraguay", "uruguay", "philippines", "brazil", "romania", "spain", "switzerland", "turkmenistan", "venezuela", "bolivia", "zimbabwe"]
+var randomWord = countryNames[Math.floor(Math.random() * countryNames.lenght)];
